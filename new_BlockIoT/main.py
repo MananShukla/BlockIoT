4a7228ee9d755d43bc54079ecbcc53b8fe2269e9
MananShukla <43486501+MananShukla@users.noreply.github.com>
2021-07-01 09:34:42 -0400

    Add files via upload

--- new_BlockIoT/main.py
+++ new_BlockIoT/main.py
@@ -10,55 +10,12 @@ import schedule
 with open(r"/Users/manan/Documents/BlockIoT/Code/new_BlockIoT/.vscode/settings.json","r") as infile:
         settings = json.load(infile)
 # Keywords such as BL_timestamp signify what type of data will be present there. 
-config= {
-    "first_name":"manan",
-    "last_name":"shukla",
-    "dob":"01-12-2001",
-    "communication":{
-        "phone":settings["personal phone number"],
-        "email":"manan.shukla2001@gmail.com",
-    },
-    "api server": "http://localhost:8000/new_BlockIoT/server_data.json",
-    "api parameters": {},
-    "template":"adherence",
-    "adherence":{
-        "medication_name":"Albuterol",
-        "Dosage":"90 mcg",
-        "Times per day":"0"
-    },
-    "identifiers":{
-        "BL_timestamp":"BL_pillstaken"
-    },
-}
-config2 = {
-    "first_name":"kavin",
-    "last_name":"shukla",
-    "dob":"01-12-2001",
-    "communication":{
-        "phone":"5162708383",
-        "email":"manan.shukla2001@gmail.com"
-    },
-    "api server": "https://ripple-health.net/api/medication/000051",
-    "api parameters": {},
-    "template":"adherence",
-    "manufacturer":"ripplehealth",
-    "adherence":{
-        "medication_name":"Vitamin D",
-        "Dosage":"10 mg",
-        "Times per day":"2"
-    },
-    "alerts":{
-        "updates":"1",
-        "reminders":"1",
-        "daily_summary":"1",
-        "long_term":"1",
-    },
-    "identifiers":{
-        "BL_timestamp":"BL_pillstaken"
-    }
-}
-#registration(config)
-registration(config2)
+
+config = list()
+with open("new_BlockIoT/configs.json") as f:
+    config = json.load(f)
+for element in config:
+    registration(element)
 
 #To view a patient's data...
 patient_1 = {
